504ce5061cd993c03658247a37463c95e59b7117
Georgemuchiri <georgw.muchiri@student.moringaschool.com>
2024-10-02 11:20:28 +0300

    Make changes

--- server/app.py
+++ server/app.py
@@ -1,23 +1,14 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
-
-
 app = Flask(__name__)
-# append to server/app.py
 
 @app.route('/')
 def index():
     return '<h1>Welcome to my page!</h1>'
 
-# modify user() in server/app.py
-
 @app.route('/<string:username>')
 def user(username):
     return f'<h1>Profile for {username}</h1>'
 
-
-# from flask import Flask
-
-# app = Flask(__name__)
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
